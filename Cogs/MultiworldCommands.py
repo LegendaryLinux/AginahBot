956167037a355f5ae1d6a7214892b349f56793a3
Chris <chris@legendserver.info>
2020-03-24 22:31:01 -0400

    Implement countdown, show-players, forfeit-player (currently broken)

--- Cogs/MultiworldCommands.py
+++ Cogs/MultiworldCommands.py
@@ -15,7 +15,7 @@ class MultiworldCommands(commands.Cog):
 
     @commands.command(
         name='broadcast',
-        brief='Broadcast a message to all players.',
+        brief='Broadcast a message to all players',
         help='Broadcast a message to all players of a multiworld game.\n'
              'Usage: !aginah broadcast {token} {message}',
     )
@@ -24,7 +24,7 @@ class MultiworldCommands(commands.Cog):
 
     @commands.command(
         name='countdown',
-        brief='Broadcast a countdown to all connected players.',
+        brief='Broadcast a countdown to all connected players',
         help='Broadcast a countdown to all players of a multiworld game.\n'
              'Usage: !aginah countdown {token} {seconds=10}',
     )
@@ -34,25 +34,52 @@ class MultiworldCommands(commands.Cog):
         token = cmd_args[2] if 0 <= 2 < len(cmd_args) else None
         seconds = cmd_args[3] if 0 <= 3 < len(cmd_args) else 10
 
+        if not token:
+            await ctx.send("You forgot to specify a game token. Use `!aginah help countdown` for more info.")
+            return
+
+        token = str(token).upper()
         if token not in ctx.bot.servers:
             await ctx.send("There is no running game with that token!")
             return
 
         # Run the countdown timer
-        asyncio.create_task(MultiServer.countdown(ctx, seconds))
+        asyncio.create_task(MultiServer.countdown(ctx.bot.servers[token]['game'], int(seconds)))
 
     @commands.command(
         name='forfeit-player',
         brief='Forfeit a player from a multiworld game',
         help='Forfeit a player from a multiworld game. This sends all items in their seed to the remaining players.\n'
-             'Usage: !aginah forfeit-player {token} {player} {team=1}',
+             'Usage: !aginah forfeit-player {token} {player_number} {team_number=1}',
     )
     async def forfeit_player(self, ctx: commands.Context):
-        pass
+        # Parse command arguments
+        cmd_args = ctx.message.content.split()
+        token = cmd_args[2] if 0 <= 2 < len(cmd_args) else None
+        player = cmd_args[3] if 0 <= 3 < len(cmd_args) else None
+        team = cmd_args[4] if 0 <= 4 < len(cmd_args) else 1
+
+        if not token:
+            await ctx.send("You forgot to specify a game token. Use `!aginah help forfeit-player` for more info.")
+            return
+
+        token = str(token).upper()
+        if token not in ctx.bot.servers:
+            await ctx.send("There is no running game with that token!")
+            return
+
+        if not player:
+            await ctx.send("You must specify a player number to forfeit. "
+                           "Use `!aginah help forfeit-player` for more info.")
+            return
+
+        # TODO: Figure out why this raises an exception
+        MultiServer.forfeit_player(ctx.bot.servers[token]['game'], int(team)-1, int(player)-1)
+        await ctx.send("Player has been forfeited.")
 
     @commands.command(
         name='give-item',
-        brief='Give an item to a player in a multiworld game',
+        brief='Give an item to a player',
         help='Give an item to a player in a multiworld game.\n'
              'Usage: !aginah give-item {token} {player} {item} {team=1}',
     )
@@ -61,12 +88,25 @@ class MultiworldCommands(commands.Cog):
 
     @commands.command(
         name='show-players',
-        brief='Show a list of all players connected to a multiworld game.',
-        help='Show a list of all players connected to a multiworld game\n'
+        brief='Show a list of all connected players',
+        help='Show a list of all players connected to a multiworld game.\n'
              'Usage: !aginah show-players {token}',
     )
     async def show_players(self, ctx: commands.Context):
-        pass
+        # Parse command arguments
+        cmd_args = ctx.message.content.split()
+        token = cmd_args[2] if 0 <= 2 < len(cmd_args) else None
+
+        if not token:
+            await ctx.send("You forgot to specify a game token. Use `!aginah help show-players` for more info.")
+            return
+
+        token = str(token).upper()
+        if token not in ctx.bot.servers:
+            await ctx.send("There is no running game with that token!")
+            return
+
+        await ctx.send(MultiServer.get_connected_players_string(ctx.bot.servers[token]['game']))
 
 
 def setup(bot: commands.Bot):
