81b81e6a8393e5f2efd9251f64811eb4f1204714
Chris Wilson <chris@legendserver.info>
2020-07-11 21:15:39 -0400

    Fix HelpfulMessages permission, add !aginah host command

--- Cogs/HelpfulMessages.py
+++ Cogs/HelpfulMessages.py
@@ -11,9 +11,17 @@ class HelpfulMessages(commands.Cog):
         brief="Get the link to the current MultiWorld setup video",
         help="Get the link to the current MultiWorld setup video"
     )
-    @commands.is_owner()
     async def get_setup_video(self, ctx: commands.Context):
         await ctx.send("https://www.youtube.com/watch?v=8g87Zhf5p_c")
+        pass
+
+    @commands.command(
+        name='host',
+        brief="Get the link to the MultiWorld WebHost service",
+        help="Get the link to the MultiWorld WebHost service"
+    )
+    async def get_webhost_link(self, ctx: commands.Context):
+        await ctx.send("https://berserkermulti.world/")
         pass
 
 
