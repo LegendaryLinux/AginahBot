9a4249844c97d8cec957724050a203c5e1bb4596
Chris Wilson <chris@legendserver.info>
2021-04-05 19:20:40 -0400

    Add an optional argument "private" to the archive command.

--- commandCategories/adminCommands.js
+++ commandCategories/adminCommands.js
@@ -6,15 +6,21 @@ module.exports = {
             description: 'Lock a channel to prevent anyone (including moderators) from sending messages or reacting.',
             longDescription: 'Lock a channel to prevent anyone (including moderators) from sending messages or ' +
               'adding reactions. If the clone argument is provided, a new duplicate channel with the original ' +
-              'permissions will be created.',
+              'permissions will be created. If the private argument is provided, the archived channel will be made' +
+              'invisible to everyone except administrators',
             aliases: [],
-            usage: '`!aginah archive [clone]`',
+            usage: '`!aginah archive [clone] [private]`',
             minimumRole: null,
             adminOnly: true,
             guildOnly: true,
             async execute(message, args) {
-                if (args.length > 0 && args[0] === 'clone') {
+                if (args.length > 0 && args.indexOf('clone') > -1) {
                     await message.channel.clone();
+                }
+
+                const denials = [ 'SEND_MESSAGES', 'ATTACH_FILES', 'ADD_REACTIONS' ];
+                if (args.length > 0 && args.indexOf('private') > -1) {
+                    denials.push('VIEW_CHANNEL');
                 }
 
                 return message.channel.edit({
@@ -22,7 +28,7 @@ module.exports = {
                     permissionOverwrites: [
                         {
                             id: message.guild.id,
-                            deny: [ 'SEND_MESSAGES', 'ATTACH_FILES', 'ADD_REACTIONS' ],
+                            deny: denials,
                         }
                     ],
                 });
