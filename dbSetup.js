3a7ec99cc69a686e5d7fc65f4d1f82e5f60c91a2
Chris Wilson <chris@legendserver.info>
2022-04-25 18:20:18 -0400

    Update dbSetup.js to use proper data types and lengths

--- dbSetup.js
+++ dbSetup.js
@@ -3,27 +3,27 @@ const config = require('./config.json');
 
 const guildData = `CREATE TABLE IF NOT EXISTS guild_data (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    guildId VARCHAR(128) NOT NULL UNIQUE,
-    moderatorRoleId VARCHAR(128) NOT NULL
+    guildId VARCHAR(64) NOT NULL UNIQUE,
+    moderatorRoleId VARCHAR(64) NOT NULL
 )`;
 
 const roleSystems = `CREATE TABLE IF NOT EXISTS role_systems (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     guildDataId BIGINT NOT NULL UNIQUE,
-    roleRequestChannelId VARCHAR(128) NOT NULL
+    roleRequestChannelId VARCHAR(64) NOT NULL
 )`;
 
 const roleCategories = `CREATE TABLE IF NOT EXISTS role_categories (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    roleSystemId VARCHAR(128) NOT NULL,
+    roleSystemId BIGINT NOT NULL,
     categoryName VARCHAR(128) NOT NULL,
-    messageId VARCHAR(128) NOT NULL
+    messageId VARCHAR(64) NOT NULL
 )`;
 
 const roles = `CREATE TABLE IF NOT EXISTS roles (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     categoryId BIGINT NOT NULL,
-    roleId VARCHAR(128) NOT NULL,
+    roleId VARCHAR(64) NOT NULL,
     roleName VARCHAR(128) NOT NULL,
     reaction VARCHAR(128) NOT NULL,
     description VARCHAR(128)
@@ -32,16 +32,16 @@ const roles = `CREATE TABLE IF NOT EXISTS roles (
 const roomSystems = `CREATE TABLE IF NOT EXISTS room_systems (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     guildDataId BIGINT NOT NULL,
-    channelCategoryId VARCHAR(128) NOT NULL,
-    newGameChannelId VARCHAR(128) NOT NULL
+    channelCategoryId VARCHAR(64) NOT NULL,
+    newGameChannelId VARCHAR(64) NOT NULL
 )`;
 
 const roomSystemGames = `CREATE TABLE IF NOT EXISTS room_system_games (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     roomSystemId BIGINT NOT NULL,
-    voiceChannelId VARCHAR(128) NOT NULL,
-    textChannelId VARCHAR(128) NOT NULL,
-    roleId VARCHAR(128) NOT NULL
+    voiceChannelId VARCHAR(64) NOT NULL,
+    textChannelId VARCHAR(64) NOT NULL,
+    roleId VARCHAR(64) NOT NULL
 )`;
 
 const roomSystemReadyChecks = `CREATE TABLE IF NOT EXISTS room_system_ready_checks (
@@ -49,7 +49,7 @@ const roomSystemReadyChecks = `CREATE TABLE IF NOT EXISTS room_system_ready_chec
     gameId BIGINT NOT NULL,
     playerId VARCHAR(64) NOT NULL,
     playerTag VARCHAR(256) NOT NULL,
-    readyState INT NOT NULL DEFAULT 0
+    readyState TINYINT NOT NULL DEFAULT 0
 )`;
 
 const scheduledEvents = `CREATE TABLE IF NOT EXISTS scheduled_events (
@@ -59,7 +59,7 @@ const scheduledEvents = `CREATE TABLE IF NOT EXISTS scheduled_events (
     channelId VARCHAR(64) NOT NULL,
     messageId VARCHAR(64) NOT NULL,
     schedulingUserId VARCHAR(64) NOT NULL,
-    schedulingUserTag VARCHAR(128) NOT NULL,
+    schedulingUserTag VARCHAR(256) NOT NULL,
     eventCode VARCHAR(6) NOT NULL
 )`;
 
@@ -70,28 +70,37 @@ const eventAttendees = `CREATE TABLE IF NOT EXISTS event_attendees (
 )`;
 
 const modContact = `CREATE TABLE IF NOT EXISTS mod_contact (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    guildDataId VARCHAR(128) NOT NULL,
-    categoryId VARCHAR(128) NOT NULL,
-    channelId VARCHAR(128) NOT NULL,
-    messageId VARCHAR(128) NOT NULL
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    guildDataId BIGINT NOT NULL,
+    categoryId VARCHAR(64) NOT NULL,
+    channelId VARCHAR(64) NOT NULL,
+    messageId VARCHAR(64) NOT NULL
 )`;
 
 const modContactChannels = `CREATE TABLE IF NOT EXISTS mod_contact_channels (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    modContactId INT NOT NULL,
-    userId VARCHAR(128) NOT NULL,
-    reportChannelId VARCHAR(128) NOT NULL,
-    reportTime VARCHAR(64) NOT NULL DEFAULT (UNIX_TIMESTAMP()),
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    modContactId BIGINT NOT NULL,
+    userId VARCHAR(64) NOT NULL,
+    reportChannelId VARCHAR(64) NOT NULL,
+    reportTime BIGINT NOT NULL DEFAULT (UNIX_TIMESTAMP()),
     resolved TINYINT(1) NOT NULL DEFAULT 0,
-    resolutionTime VARCHAR(64)
+    resolutionTime BIGINT
 )`;
 
 const botOptions = `CREATE TABLE IF NOT EXISTS bot_options (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     guildDataId BIGINT NOT NULL,
     name VARCHAR(64) NOT NULL,
     value VARCHAR(128) NOT NULL
+)`;
+
+const messageTags = `CREATE TABLE IF NOT EXISTS message_tags (
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    guildDataId BIGINT NOT NULL,
+    tagName VARCHAR(32) NOT NULL,
+    tagContent TEXT(1024) NOT NULL,
+    createdByUserId VARCHAR(64) NOT NULL,
+    createdTime BIGINT NOT NULL DEFAULT (UNIX_TIMESTAMP())
 )`;
 
 const db = mysql.createConnection({
@@ -103,11 +112,7 @@ const db = mysql.createConnection({
     bigNumberStrings: true,
 });
 
-const handler = (err) => {
-    if (err) {
-        console.log(err);
-    }
-};
+const handler = (err) => console.log(err);
 
 db.query(guildData, handler);
 db.query(roleSystems, handler);
@@ -121,4 +126,5 @@ db.query(eventAttendees, handler);
 db.query(modContact, handler);
 db.query(modContactChannels, handler);
 db.query(botOptions, handler);
+db.query(messageTags, handler);
 db.end();
