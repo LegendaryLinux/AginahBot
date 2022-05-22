21e91cf08b556e9712ee194f6db419691dae2707
Chris Wilson <chris@legendserver.info>
2022-05-22 18:28:59 -0400

    Add save-log command to Utility

--- commandCategories/utility.js
+++ commandCategories/utility.js
@@ -1,6 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 const { parseTimeString } = require('../lib');
 const { generalErrorHandler } = require('../errorHandlers');
+const tmp = require('tmp');
+const fs = require('fs');
 
 const sendTimestampMessage = async (message, targetDate) => {
     const embed = new MessageEmbed()
@@ -32,7 +34,7 @@ module.exports = {
             guildOnly: false,
             moderatorRequired: false,
             adminOnly: false,
-            execute(message, args) {
+            async execute(message, args) {
                 if (args.length === 0) {
                     return message.channel.send("You must specify a date/time value.");
                 }
@@ -48,6 +50,83 @@ module.exports = {
                     }
                     generalErrorHandler(error);
                 }
+            }
+        },
+        {
+            name: 'save-log',
+            description: 'Save a log of recent channel messages to a text file.',
+            longDescription: 'Save a log of up to 1000 recent channel messages to a text file. ' +
+              'Defaults to one hundred messages.',
+            aliases: [],
+            usage: '`!aginah save-log [limit]',
+            guildOnly: true,
+            moderatorRequired: true,
+            adminOnly: false,
+            async execute(message, args) {
+                if ((args.length > 0) && (!args[0].match(/^\d+$/))) {
+                    return message.channel.send('Limit argument must be an integer from 1 to 1000.');
+                }
+
+                // Control variables
+                const logLimit = (args.length > 0) ? parseInt(args[0], 10) : 100;
+                const logs = [];
+                let lastMessageId = message.id;
+
+                // Do not fetch more than 1000 messages from the Discord API
+                if (logLimit < 1 || logLimit > 1000) {
+                    return message.channel.send('Limit argument must be an integer from 1 to 1000.');
+                }
+
+                while (logs.length < logLimit) {
+                    // Determine number of messages to be fetched this request
+                    const fetchLimit = ((logLimit - logs.length) > 100) ? 100 : (logLimit - logs.length);
+
+                    // Fetch messages from Discord API
+                    const messages = await message.channel.messages.fetch({
+                        before: lastMessageId,
+                        limit: fetchLimit,
+                    });
+
+                    // Save relevant message data
+                    messages.each((msg) => {
+                        logs.push({
+                            id: msg.id,
+                            user: `${msg.author.username}#${msg.author.discriminator}`,
+                            timestamp: msg.createdTimestamp,
+                            content: msg.content,
+                        });
+                    });
+
+                    // Begin fetching from the earliest message
+                    lastMessageId = logs[logs.length - 1].id;
+
+                    // If no more messages are available, stop fetching
+                    if (messages.size <= fetchLimit) { break; }
+                }
+
+                // Reverse the array so the oldest messages accessed first, and will therefore be printed earlier
+                // in the output file
+                logs.reverse();
+
+                // Build output file
+                let output = '';
+                logs.forEach((log) => {
+                    output += `${log.user} (${new Date(log.timestamp).toUTCString()}):\n${log.content}\n\n`;
+                });
+
+                // Save the output to a temporary file and send it to the channel
+                return tmp.file((err, tmpFilePath, fd, cleanupCallback) => {
+                    fs.writeFile(tmpFilePath, output, () => {
+                        return message.channel.send({
+                            files: [
+                                {
+                                    name: `${message.channel.name}-log.txt`,
+                                    attachment: tmpFilePath,
+                                }
+                            ]
+                        });
+                    });
+                });
             }
         },
     ],
