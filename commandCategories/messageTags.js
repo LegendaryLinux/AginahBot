91596fa3c90d889936e04c18b1333b113f75362f
Chris Wilson <chris@legendserver.info>
2022-12-30 19:24:50 -0500

    Convert messageTags.js to slash command format. Remove help command category.

--- commandCategories/messageTags.js
+++ commandCategories/messageTags.js
@@ -1,64 +1,61 @@
 const { dbQueryOne, dbQueryAll, dbExecute } = require('../lib');
-
-// TODO: Convert to slash commands
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   category: 'Message Tags',
   commands: [
     {
-      name: 'set-tag',
-      description: 'Add or update a &tag for a server.',
-      longDescription: 'Add or update a tag for a server. If a user includes this tag in any part of ' +
-              'a message, the bot will respond with the content of this tag. Tags may contain only letters, ' +
-              'numbers, and underscores.',
-      aliases: [],
-      usage: '`!aginah set-tag tagName content`',
-      moderatorRequired: true,
-      adminOnly: false,
-      guildOnly: true,
-      async execute(message, args) {
-        if (args.length < 2) {
-          return message.channel.send('Invalid arguments provided. See `!aginah help set-tag`.');
-        }
+      commandBuilder: new SlashCommandBuilder()
+        .setName('tagAdd')
+        .setDescription('Add or update a tag for a server. If a user includes this tag in any part of ' +
+          'a message, the bot will respond with the content of this tag. Tags may contain only letters, ' +
+          'numbers, and underscores.')
+        .addStringOption((opt) => opt
+          .setName('tagName')
+          .setDescription('The tag to respond to')
+          .setRequired(true))
+        .addStringOption((opt) => opt
+          .setName('content')
+          .setDescription('The text which will be displayed when a tag is found in a message')
+          .setRequired(true))
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(0),
+      async execute(interaction) {
+        const tagName = interaction.options.getString('tagName');
+        const content = interaction.options.getString('content');
 
         // Tags may contain only alphanumeric characters
-        if (args[0].search(/\W/) > -1) {
-          return message.channel.send('Tag names may contain only letters, numbers, and underscores.');
+        if (tagName.search(/\W/) > -1) {
+          return interaction.reply('Tag names may contain only letters, numbers, and underscores.');
         }
 
-        // The full message after the tag name should be used at the tag's content. Remove the tag name
-        // and trim the remaining text from the message.
-        const tagContent = message.content.substring(message.content.search(args[0]) + args[0].length);
-
         // Fetch guildDataId
-        const guildData = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', [message.guild.id]);
+        const guildData = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', [interaction.guildId]);
 
         // Save new tag to database
         let sql = `REPLACE INTO message_tags (guildDataId, tagName, tagContent, createdByUserId)
                            VALUES (?,?,?,?)`;
-        await dbExecute(sql, [guildData.id, args[0].toLowerCase(), tagContent, message.member.id]);
+        await dbExecute(sql, [guildData.id, tagName.toLowerCase(), content, interaction.user.id]);
 
-        return message.channel.send(`Set content for tag \`${args[0].toLowerCase()}\`.`);
+        return interaction.reply(`Set content for tag \`${tagName.toLowerCase()}\`.`);
       },
     },
     {
-      name: 'delete-tag',
-      description: 'Remove a &tag from a server.',
-      longDescription: null,
-      aliases: [],
-      usage: '`!aginah delete-tag tagName`',
-      moderatorRequired: true,
-      adminOnly: false,
-      guildOnly: true,
-      async execute(message, args) {
-        if (args.length < 1) {
-          return message.channel.send('Invalid arguments provided. See `!aginah help delete-tag`.');
-        }
+      commandBuilder: new SlashCommandBuilder()
+        .setName('tagDelete')
+        .setDescription('Remove a &tag from a server.')
+        .addStringOption((opt) => opt
+          .setName('tagName')
+          .setRequired(true))
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(0),
+      async execute(interaction) {
+        const tagName = interaction.options.getString('tagName');
 
         // Fetch guildDataId
-        const guildData = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', [message.guild.id]);
+        const guildData = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', [interaction.guildId]);
         if (!guildData) {
-          return message.channel.send('That tag does not exist on this server.');
+          return interaction.reply('That tag does not exist on this server.');
         }
 
         // If the tag does not exist, inform the user
@@ -67,39 +64,35 @@ module.exports = {
                    JOIN guild_data gd ON mt.guildDataId = gd.id
                    WHERE gd.guildId=?
                     AND mt.tagName=?`;
-        const existing = await dbQueryOne(sql, [message.guild.id, args[0].toLowerCase()]);
+        const existing = await dbQueryOne(sql, [interaction.guildId, tagName.toLowerCase()]);
         if (!existing) {
-          return message.channel.send('That tag does not exist on this server.');
+          return interaction.reply('That tag does not exist on this server.');
         }
 
         // Delete the tag from the database
         await dbExecute('DELETE FROM message_tags WHERE guildDataId=? AND tagName=?',
-          [guildData.id, args[0].toLowerCase()]
+          [guildData.id, tagName.toLowerCase()]
         );
 
-        return message.channel.send(`Deleted tag \`${args[0].toLowerCase()}\`.`);
+        return interaction.reply(`Deleted tag \`${tagName.toLowerCase()}\`.`);
       }
     },
     {
-      name: 'tags',
-      description: 'List all &tags available on this server.',
-      longDescription: null,
-      aliases: [],
-      usage: '`!aginah tags',
-      moderatorRequired: false,
-      adminOnly: false,
-      guildOnly: true,
-      async execute(message, args) {
+      commandBuilder: new SlashCommandBuilder()
+        .setName('tagList')
+        .setDescription('List all &tags available on this server.')
+        .setDMPermission(false),
+      async execute(interaction) {
         // Fetch guild tags
         let sql = `SELECT mt.tagName
                    FROM message_tags mt
                    JOIN guild_data gd ON mt.guildDataId = gd.id
                    WHERE gd.guildId=?`;
-        const tags = await dbQueryAll(sql, [message.guild.id]);
+        const tags = await dbQueryAll(sql, [interaction.guildId]);
 
         // If there are no tags available for this guild, inform the user
         if (tags.length === 0) {
-          return message.channel.send('There are currently no tags available on this server.');
+          return interaction.reply('There are currently no tags available on this server.');
         }
 
         // Build a string containing all the tag names surrounded by backticks
@@ -108,7 +101,7 @@ module.exports = {
         tagString = tagString.slice(0, -2);
 
         // Send the list of tags to the user
-        return message.channel.send(`The following tags are available on this server:\n${tagString}`);
+        return interaction.reply(`The following tags are available on this server:\n${tagString}`);
       }
     }
   ],
