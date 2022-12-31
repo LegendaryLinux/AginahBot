3d814ed44ad1a6f6dc588b1e30220c00c851bfb2
Chris Wilson <chris@legendserver.info>
2022-12-30 19:33:27 -0500

    Convert modContact.js to slash command format.

--- commandCategories/modContact.js
+++ commandCategories/modContact.js
@@ -1,50 +1,46 @@
 const { dbQueryOne, dbExecute } = require('../lib');
-const { ButtonBuilder, ButtonStyle, ChannelType, PermissionsBitField, ActionRowBuilder } = require('discord.js');
-
-// TODO: Convert to slash commands
+const { ButtonBuilder, ButtonStyle, ChannelType, PermissionsBitField, ActionRowBuilder,
+  SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   category: 'Mod Contact',
   commands: [
     {
-      name: 'init-mod-contact',
-      description: 'Enable the Mod Contact feature in a Discord server.',
-      longDescription: 'Enable the Mod Contact feature in a Discord server. This will create a category ' +
-              'containing a single channel with a message where users can click on a button to contact the mods.',
-      aliases: [],
-      usage: '`!aginah init-mod-contact`',
-      moderatorRequired: false,
-      adminOnly: true,
-      guildOnly: true,
-      async execute(message) {
+      commandBuilder: new SlashCommandBuilder()
+        .setName('modContactEnable')
+        .setDescription('Enable the Mod Contact feature in a Discord server. This will create a category ' +
+          'containing a single channel with a message where users can click on a button to contact the mods.')
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(0),
+      async execute(interaction) {
         // If the Mod Contact feature has already been enabled for this guild, do nothing
-        let guildDataId = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', message.guild.id);
+        let guildDataId = await dbQueryOne('SELECT id FROM guild_data WHERE guildId=?', interaction.guildId);
         guildDataId = guildDataId.id;
 
         const existing = await dbQueryOne('SELECT 1 FROM mod_contact WHERE guildDataId=?', [guildDataId]);
         if (existing) {
-          return message.channel.send('The Mod Contact feature is already enabled for this server!');
+          return interaction.reply('The Mod Contact feature is already enabled for this server!');
         }
 
         // Create a category to contain the report channels
-        const category = await message.guild.channels.create({
+        const category = await interaction.guild.channels.create({
           name: 'Mod Contact',
           type: ChannelType.GuildCategory,
           permissionOverwrites: [
             {
               // @everyone may not send messages or add reactions
-              id: message.guild.id,
+              id: interaction.guildId,
               deny: [ PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.AddReactions ],
             },
             {
               // @AginahBot may post in this category
-              id: message.client.user.id,
+              id: interaction.client.user.id,
               allow: [ PermissionsBitField.Flags.SendMessages ],
             },
           ],
         });
 
-        const channel = await message.guild.channels.create({
+        const channel = await interaction.guild.channels.create({
           name: 'mod-contact',
           type: ChannelType.GuildText,
           topic: 'Privately contact the moderator team.',
@@ -52,12 +48,12 @@ module.exports = {
           permissionOverwrites: [
             {
               // @everyone may not send messages or add reactions
-              id: message.guild.id,
+              id: interaction.guildId,
               deny: [ PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.AddReactions ],
             },
             {
               // @AginahBot may post in this channel
-              id: message.client.user.id,
+              id: interaction.client.user.id,
               allow: [ PermissionsBitField.Flags.SendMessages ],
             },
           ],
@@ -79,41 +75,40 @@ module.exports = {
 
         // Update database with mod_contact data
         let sql = 'INSERT INTO mod_contact (guildDataId, categoryId, channelId, messageId) VALUES (?, ?, ?, ?)';
-        return dbExecute(sql, [ guildDataId, category.id, channel.id, contactMessage.id ]);
+        await dbExecute(sql, [ guildDataId, category.id, channel.id, contactMessage.id ]);
+        return interaction.reply('Mod contact feature enabled.');
       },
     },
     {
-      name: 'destroy-mod-contact',
-      description: 'Remove the Mod Contact feature from a Discord server.',
-      longDescription: 'Remove the Mod Contact feature from a Discord server. This also removes all ' +
-              'Mod Contact history.',
-      aliases: [],
-      usage: '`!aginah destroy-mod-contact`',
-      moderatorRequired: false,
-      adminOnly: true,
-      guildOnly: true,
-      async execute(message) {
+      commandBuilder: new SlashCommandBuilder()
+        .setName('modContactDisable')
+        .setDescription('Remove the Mod Contact feature from a Discord server. This also removes all ' +
+          'Mod Contact history.')
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(0),
+      async execute(interaction) {
         // Fetch information about this guild's mod-contact feature
         let sql = `SELECT mc.id, mc.categoryId
                            FROM mod_contact mc
                            JOIN guild_data gd ON mc.guildDataId = gd.id
                            WHERE gd.guildId = ?`;
-        const modContact = await dbQueryOne(sql, [ message.guild.id ]);
+        const modContact = await dbQueryOne(sql, [ interaction.guildId ]);
 
         // If the feature isn not enabled, alert the user
         if (!modContact) {
-          return message.channel.send('The Mod Contact feature is not enabled for this server.');
+          return interaction.reply('The Mod Contact feature is not enabled for this server.');
         }
 
         // Delete entries for previous mod contact events
         await dbExecute('DELETE FROM mod_contact_channels WHERE modContactId=?', [ modContact.id ]);
 
         // Delete the guild category and channels
-        const category = await message.guild.channels.fetch(modContact.categoryId);
+        const category = await interaction.guild.channels.fetch(modContact.categoryId);
         await category.children.cache.each(async (child) => await child.delete());
         await category.delete();
 
-        return dbExecute('DELETE FROM mod_contact WHERE id=?', [ modContact.id ]);
+        await dbExecute('DELETE FROM mod_contact WHERE id=?', [ modContact.id ]);
+        return interaction.reply('Mod contact feature disabled.');
       },
     }
   ],
