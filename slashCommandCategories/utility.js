ae316ad8700138dfc2baffa91f36801341f370cf
Chris Wilson <chris@legendserver.info>
2023-02-18 17:23:17 -0500

    Improve error handling for utility commands

--- slashCommandCategories/utility.js
+++ slashCommandCategories/utility.js
@@ -30,58 +30,67 @@ module.exports = {
           });
         }
 
-        while (logs.length < limit) {
-          // Determine number of messages to be fetched this request
-          const fetchLimit = ((limit - logs.length) > 100) ? 100 : (limit - logs.length);
+        try{
+          // This might take a few seconds
+          await interaction.deferReply({ ephemeral: true });
 
-          // Fetch messages from Discord API
-          const messages = await interaction.channel.messages.fetch({
-            before: lastMessageId,
-            limit: fetchLimit,
+          while (logs.length < limit) {
+            // Determine number of messages to be fetched this request
+            const fetchLimit = ((limit - logs.length) > 100) ? 100 : (limit - logs.length);
+
+            // Fetch messages from Discord API
+            const messages = await interaction.channel.messages.fetch({
+              before: lastMessageId,
+              limit: fetchLimit,
+            });
+
+            // Save relevant message data
+            messages.each((msg) => {
+              logs.push({
+                id: msg.id,
+                user: `${msg.author.username}#${msg.author.discriminator}`,
+                timestamp: msg.createdTimestamp,
+                content: msg.content,
+              });
+            });
+
+            // Begin fetching from the earliest message
+            lastMessageId = logs[logs.length - 1].id;
+
+            // If no more messages are available, stop fetching
+            if (messages.size <= fetchLimit) { break; }
+          }
+
+          // Reverse the array so the oldest messages occur first, and will therefore be printed earlier
+          // in the output file
+          logs.reverse();
+
+          // Build output file
+          let output = '';
+          logs.forEach((log) => {
+            output += `${log.user} (${new Date(log.timestamp).toUTCString()}):\n${log.content}\n\n`;
           });
 
-          // Save relevant message data
-          messages.each((msg) => {
-            logs.push({
-              id: msg.id,
-              user: `${msg.author.username}#${msg.author.discriminator}`,
-              timestamp: msg.createdTimestamp,
-              content: msg.content,
+          // Save the output to a temporary file and send it to the channel
+          return tmp.file((err, tmpFilePath, fd, cleanupCallback) => {
+            fs.writeFile(tmpFilePath, output, () => {
+              return interaction.followUp({
+                ephemeral: true,
+                content: `Saved a log of the previous ${limit} messages in this channel.`,
+                files: [
+                  {
+                    name: `${interaction.channel.name}-log.txt`,
+                    attachment: tmpFilePath,
+                  }
+                ]
+              });
             });
           });
-
-          // Begin fetching from the earliest message
-          lastMessageId = logs[logs.length - 1].id;
-
-          // If no more messages are available, stop fetching
-          if (messages.size <= fetchLimit) { break; }
+        } catch (e) {
+          console.error(e);
+          return interaction.followUp('Something went wrong and the Mod Contact feature could not be enabled ' +
+            'on this server. Please report this bug on [AginahBot\'s Discord](https://discord.gg/2EZNrAw9Ja)');
         }
-
-        // Reverse the array so the oldest messages accessed first, and will therefore be printed earlier
-        // in the output file
-        logs.reverse();
-
-        // Build output file
-        let output = '';
-        logs.forEach((log) => {
-          output += `${log.user} (${new Date(log.timestamp).toUTCString()}):\n${log.content}\n\n`;
-        });
-
-        // Save the output to a temporary file and send it to the channel
-        return tmp.file((err, tmpFilePath, fd, cleanupCallback) => {
-          fs.writeFile(tmpFilePath, output, () => {
-            return interaction.reply({
-              ephemeral: true,
-              content: `Saved a log of the previous ${limit} messages in this channel.`,
-              files: [
-                {
-                  name: `${interaction.channel.name}-log.txt`,
-                  attachment: tmpFilePath,
-                }
-              ]
-            });
-          });
-        });
       }
     },
   ],
