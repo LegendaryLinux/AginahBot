2bdc73b9af461c021904806d17a754565b7c3c41
Chris Wilson <chris@legendserver.info>
2023-06-20 23:59:49 -0400

    Remove unused lib function

--- lib.js
+++ lib.js
@@ -449,16 +449,4 @@ module.exports = {
       });
     }
   },
-
-  getDisplayName: (userLikeObject) => {
-    if (
-      userLikeObject.username && userLikeObject.displayName &&
-      (userLikeObject.displayName === userLikeObject.displayName) &&
-      userLikeObject.globalName
-    ) {
-      return userLikeObject.globalName;
-    }
-
-    return userLikeObject.displayName || userLikeObject.globalName || userLikeObject.username;
-  },
 };
