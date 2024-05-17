31b322ca13834720f22f0a07917e6ffde5394bcb
Chris Wilson <chris@legendserver.info>
2024-05-17 00:38:54 -0400

    Catch an unhandled exception which could occur if the database believes a mod-contact channel is already open, but the Discord API is unable to fetch the channel

--- interactionListeners/modContactListener.js
+++ interactionListeners/modContactListener.js
@@ -1,4 +1,5 @@
 const { modContactEnabled, modContactExists, createModContact } = require('../slashCommandCategories/modContact');
+const { dbExecute } = require('../lib');
 
 module.exports = async (client, interaction) => {
   // Only listen for button interactions
@@ -14,14 +15,26 @@ module.exports = async (client, interaction) => {
     return interaction.update({});
   }
 
-  // Inform the user if a mod-contact already exists
+  // Check if the DB believes a mod-contact exists for this user already
   const existingChannelId = await modContactExists(interaction.guild, interaction.member);
   if (existingChannelId) {
-    const existingChannel = await interaction.guild.channels.fetch(existingChannelId);
-    await interaction.user.send(`You already have a mod-contact open here: ${existingChannel}`);
+    try {
+      // Attempt to fetch this channel from the Discord API and inform the user a mod-contact already exists
+      const existingChannel = await interaction.guild.channels.fetch(existingChannelId);
+      await interaction.user.send(`You already have a mod-contact open here: ${existingChannel}`);
 
-    // Inform Discord the interaction was handled, but do not change the original message
-    return interaction.update({});
+      // Inform Discord the interaction was handled, but do not change the original message
+      return interaction.update({});
+    } catch (e) {
+      // The database claims this channel exists, but the Discord API disagrees. Set this mod contact as
+      // resolved and allow the system to open a new one. This can happen if a mod-contact user channel
+      // is manually deleted, or if Discord is just having a bad day
+      let sql = `UPDATE mod_contact_channels
+                 SET resolved=1, resolutionTime=UNIX_TIMESTAMP()
+                 WHERE userId=?
+                   AND reportChannelId=?`;
+      await dbExecute(sql, [interaction.user.id, existingChannelId]);
+    }
   }
 
   // Create the mod-contact channel
